9e635f947048839c4ff761b7464ed328052819b2
Loukil Med Ghofran <loukilmedgofran@gmail.com>
2018-11-15 21:50:43 +0100

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { FooterComponent } from './footer/footer.component';
-import { SliderComponent } from './slider/slider.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
-import { HomeComponent } from './home/home.component';
-import { MessagesFormComponent } from './messages-form/messages-form.component';
-import { ContactHeaderComponent } from './contact-header/contact-header.component';
-import { ProductsComponent } from './products/products.component';
-import { MiddleProductsComponent } from './middle-products/middle-products.component';
-import { BestSellersComponent } from './best-sellers/best-sellers.component';
-import { IconBoxComponent } from './icon-box/icon-box.component';
-import { ProductSheetComponent } from './product-sheet/product-sheet.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { RelatedProductsComponent } from './related-products/related-products.component';
-import { CartComponent } from './cart/cart.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { ProductThumbnailComponent } from './product-thumbnail/product-thumbnail.component';
-
+import { AppComponent } from "./app.component";
+import { MainNavComponent } from "./main-nav/main-nav.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SliderComponent } from "./slider/slider.component";
+import { ContactPageComponent } from "./contact-page/contact-page.component";
+import { HomeComponent } from "./home/home.component";
+import { MessagesFormComponent } from "./messages-form/messages-form.component";
+import { ContactHeaderComponent } from "./contact-header/contact-header.component";
+import { ProductsComponent } from "./products/products.component";
+import { MiddleProductsComponent } from "./middle-products/middle-products.component";
+import { BestSellersComponent } from "./best-sellers/best-sellers.component";
+import { IconBoxComponent } from "./icon-box/icon-box.component";
+import { ProductSheetComponent } from "./product-sheet/product-sheet.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { RelatedProductsComponent } from "./related-products/related-products.component";
+import { CartComponent } from "./cart/cart.component";
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from "./login/login.component";
+import { ProductThumbnailComponent } from "./product-thumbnail/product-thumbnail.component";
 
 const appRoutes: Routes = [
-  { path: 'contact-page', component: ContactPageComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'home/product-sheet', component: ProductSheetComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
+  { path: "contact-page", component: ContactPageComponent },
+  { path: "home", component: HomeComponent },
+  { path: "home/product-sheet", component: ProductSheetComponent },
+  { path: "cart", component: CartComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent }
 ];
-
-
-
 
 @NgModule({
   declarations: [
@@ -70,4 +66,4 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
