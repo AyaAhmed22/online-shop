f4b126d79067628f2268e843d92caccf09d66966
Loukil Med Ghofran <loukilmedgofran@gmail.com>
2018-11-15 21:01:22 +0100

    changed products list to be displayed from an angular object for later use

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,12 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  product = {
-    name: "iPhone XS",
-    price:"670",
-    status:"Sale",
-    image:"assets/images/iphone_xs.png"
-  }
+  products = [
+    {
+      name: "iPhone XS",
+      price:"670",
+      status:"Sale",
+      image:"assets/images/iphone_xs.png"
+    },
+    {
+      name: "iPod Touch",
+      price:"670",
+      status:"",
+      image:"assets/images/ipod_touch.jpg"
+    },
+    {
+      name: "iPhone XS",
+      price:"670",
+      status:"Sale",
+      image:"assets/images/iphone_xs.png"
+    },
+    {
+      name: "Samsung Tab S4",
+      price:"570",
+      status:"",
+      image:"assets/images/samsung_galaxy_tab_s4.jpg"
+    },
+    {
+      name: "iPad Pro",
+      price:"1100",
+      status:"",
+      image:"assets/images/ipad_pro.jpg"
+    },
+    {
+      name: "ASUS ChromeBook Flip C101PA",
+      price:"800",
+      status:"Hot",
+      image:"assets/images/asus_chromebook_flip.jpg"
+    },
+    {
+      name: "Huawei Nova 3i",
+      price:"300",
+      status:"",
+      image:"assets/images/huawei_nova_3i.jpg"
+    },
+    {
+      name: "Samsung Gear Sport",
+      price:"670",
+      status:"Hot",
+      image:"assets/images/gear_sport.jpg"
+    },
+  ]
 
   constructor() { }
 
