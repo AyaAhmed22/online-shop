93f9a9ac8e30bed7e61b4a64247326587487fe8c
Loukil Med Ghofran <loukilmedgofran@gmail.com>
2018-11-20 21:00:11 +0100

    added search panel

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent implements OnInit {
+  private IsSearching: boolean = false;
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) { }
+
+  private setIsSearching(): void {
+    this.IsSearching = this.IsSearching == false ? true : false;
+    this.cd.detectChanges();
+  }
+
+  getIsSearching(): boolean {
+    return this.IsSearching;
+  }
+
+  private hideSearchPane(): boolean {
+    if (this.IsSearching) {
+      return true;
+    }
+  }
+
+  private searchElement() {
+    alert('not yet implemented ')
+  }
 
   ngOnInit() {
   }
